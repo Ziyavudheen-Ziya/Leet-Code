25b51f28f02f8b5522babdd5d533d0775cb9477b
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-07-09 10:28:21 +0530

    leet2154

---
+++ leet2154.js
@@ -0,0 +1,13 @@
+let numBottles = 9,
+  numExchange = 3;
+let sum = numBottles;
+
+while (numBottles >= numExchange) {
+  let newBottles = Math.floor(numBottles / numExchange);
+  let reamaingBottles = numBottles % numExchange;
+  sum += newBottles;
+
+  numBottles = newBottles + reamaingBottles;
+}
+
+console.log(sum);
