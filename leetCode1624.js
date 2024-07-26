37271dd2b12a8bc630e5b1ab0f3024fc60e632d8
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-07-26 11:34:38 +0530

    leetcode

--- leetCode1624.js
+++ leetCode1624.js
@@ -3,20 +3,14 @@ let obj = {};
 let arr = [];
 let c = 0;
 
+let maxDistance = -1;
 
-let maxDistance = -1
-
-for(let i=0;i<s.length;i++){
-
-     for(let j=i+1;j<s.length;j++){
-
-         if(s[i]===s[j]){
-
-            
-               maxDistance = Math.max(maxDistance , j-i-1)
-            
-         }
-     }
+for (let i = 0; i < s.length; i++) {
+  for (let j = i + 1; j < s.length; j++) {
+    if (s[i] === s[j]) {
+      maxDistance = Math.max(maxDistance, j - i - 1);
+    }
+  }
 }
 
 console.log(maxDistance);
