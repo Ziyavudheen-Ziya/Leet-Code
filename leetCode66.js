f0d3a6e312a6b1811897bbe8dcbf739ddb72b71e
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-09-01 22:32:46 +0530

    leetcode66.js

---
+++ leetCode66.js
@@ -0,0 +1,16 @@
+let digits = [9];
+
+for (let i = digits.length - 1; i >= 0; i--) {
+  if (digits[i] < 9) {
+    digits[i]++;
+    break;
+  }
+
+  digits[i] = 0;
+}
+
+if (digits[0] == 0) {
+  digits.unshift(1);
+}
+
+console.log(digits);
