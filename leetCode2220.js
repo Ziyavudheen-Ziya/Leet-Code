c17ddca14aa7875d88546debcc71c5f952fa6d02
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-09-11 10:20:45 +0530

    leetCide2220.js

--- leetCode2220.js
+++ leetCode2220.js
@@ -1,32 +1,17 @@
-let start = 10, goal = 7
-let c =0
+let start = 10, goal = 7;
+let c = 0;
 
-let startBinary = start.toString(2)
-let goalBinary = goal.toString(2)
+let startBinary = start.toString(2);
+let goalBinary = goal.toString(2);
 
+const maxLength = Math.max(startBinary.length, goalBinary.length);
+startBinary = startBinary.padStart(maxLength, "0");
+goalBinary = goalBinary.padStart(maxLength, "0");
 
-const maxLength = Math.max(startBinary.length,goalBinary.length)
-startBinary = startBinary.padStart(maxLength,'0')
-goalBinary = goalBinary.padStart(maxLength,'0')
-
-
-
-
-
-
-for(let i=0;i<maxLength;i++){
-
-    if(startBinary[i]!=goalBinary[i]){
-
-        c++;
-    }
+for (let i = 0; i < maxLength; i++) {
+  if (startBinary[i] != goalBinary[i]) {
+    c++;
+  }
 }
 
 console.log(c);
-
-
-    
-
-    
-     
-     
