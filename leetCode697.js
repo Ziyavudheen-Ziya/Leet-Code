4b459bbed1fde49c77686fbbaedbb5878725dab5
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-12-05 10:00:28 +0530

    leetCode697.js

---
+++ leetCode697.js
@@ -0,0 +1,19 @@
+let s = "abcde",
+  goal = "cdeab";
+
+let rotateStr = false;
+
+if (s.length !== goal.length) {
+  console.log(false);
+}
+
+for (let i = 0; i < s.length; i++) {
+  let val = s[0];
+  s = s.slice(1) + val;
+
+  if (s === goal) {
+    rotateStr = true;
+    break;
+  }
+}
+console.log(rotateStr);
