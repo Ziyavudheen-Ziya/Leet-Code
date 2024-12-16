9abf39006f049adc5407fcad11edb1e79d224373
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-12-16 13:57:36 +0530

    leetCode686.js

---
+++ leetCode686.js
@@ -0,0 +1,33 @@
+let  a = "a", b = "aa"
+
+let subString = ''
+let repeatTimes = 0
+while(subString.length<b.length){
+
+    subString+=a
+    
+    repeatTimes++;
+
+}
+
+
+if(subString.includes(b)){
+
+    console.log(repeatTimes);
+    
+}else{
+
+    subString+=a
+    repeatTimes++;
+
+    if(subString.includes(b)){
+
+        console.log(repeatTimes);
+        
+    }else{
+        console.log(-1);
+        
+    }
+}
+
+
