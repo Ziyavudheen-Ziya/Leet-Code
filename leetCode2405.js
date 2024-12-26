01b679ab381a7e87c3344e1ece3719455ffd0b9a
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-12-26 20:16:09 +0530

    leetCode2405.js

---
+++ leetCode2405.js
@@ -0,0 +1,22 @@
+let s = "ssssss".split("");
+
+let res = [];
+
+let sub = [];
+
+let c = 0;
+for (let i = 0; i < s.length; i++) {
+  if (!sub.includes(s[i])) {
+    sub.push(s[i]);
+  } else {
+    res.push(sub.join(""));
+    sub = [s[i]];
+  }
+}
+
+if (sub.length > 0) {
+  res.push(sub.join(""));
+}
+
+
+console.log(res.length);
