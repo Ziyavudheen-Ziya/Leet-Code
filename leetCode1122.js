53fd1151defd10e7e0298e29ab3308756d9a53fe
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2024-12-29 22:46:52 +0530

    leetCode1122.js

--- leetCode1122.js
+++ leetCode1122.js
@@ -1,6 +1,6 @@
 let arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19],
   arr2 = [2, 1, 4, 3, 9, 6];
-  
+
 let toAscending = [];
 let sortedArr = [];
 let restOfValues = [];
@@ -14,8 +14,6 @@ for (let i = 0; i < arr2.length; i++) {
     }
   }
 }
-
-
 
 for (let i = 0; i < arr1.length; i++) {
   if (!sortedArr.includes(arr1[i])) {
