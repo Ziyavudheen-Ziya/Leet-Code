4562924939ecbedf4dc3fb9e07d894813ff235b6
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2025-05-06 20:21:39 +0530

    leetCode1512.js

--- leetCode1512.js
+++ leetCode1512.js
@@ -4,7 +4,7 @@ for(let i=0;i<nums.length;i++){
 
       for(let j=i+1;j<nums.length;j++){
 
-              if(nums[i]===nums[j]&&i<j){
+              if(nums[i]===nums[j]){
 
                   c++;
 
