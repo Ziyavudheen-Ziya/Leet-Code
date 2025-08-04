7b09ea92d488b89f063fab0b0a5cfdb089be3923
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2025-08-04 13:31:21 +0530

    leetCode189.js

---
+++ leetCode189.js
@@ -0,0 +1,18 @@
+let nums = [-1],
+  k = 2;
+
+function reverse(nums, start, end) {
+  while (start < end) {
+    [nums[start], nums[end]] = [nums[end], nums[start]];
+
+    start++;
+    end--;
+  }
+}
+
+reverse(nums, 0, nums.length - 1);
+
+reverse(nums, 0, k - 1);
+
+reverse(nums, k, nums.length - 1);
+console.log(nums, "tota");
