ff40054b34d0fa719ef33d2a05041f29f49ce3a4
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2025-09-22 21:09:42 +0530

    leetCode2657.js

--- leetCode2657.js
+++ leetCode2657.js
@@ -1,16 +1,27 @@
-let A = [2,3,1], B = [3,1,2]
+let A = [2, 3, 1],
+  B = [3, 1, 2];
 
 let result = new Array(A.length).fill(0);
 
-let obj = {};
-let length = A.length;
-for (let i = 0; i < length; i++) {
-  for (let j = 0; j <= i; j++) {
-    for (let k = 0; k <= i; k++) {
-      if (A[j] === B[k]) {
-        result[i]++;
-      }
+let n = A.length;
+let i = 0;
+while (true) {
+  let count = 0;
+  let arrA = A.slice(0, i + 1);
+  let arrB = B.slice(0, i + 1);
+
+  for (let j = 0; j < arrA.length; j++) {
+    if (arrB.includes(arrA[j])) {
+      count++;
     }
   }
+  result[i] = count;
+
+  if (i < n - 1) {
+    i++;
+  } else {
+    break;
+  }
 }
+
 console.log(result);
