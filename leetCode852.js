fd4a08a471c4fc2278a2577ff68de0641fd65254
Muhammed Ziyavudheen <ziyavudheen1234@gmail.com>
2025-10-17 09:25:38 +0530

    leetCode852.js

--- leetCode852.js
+++ leetCode852.js
@@ -1,15 +1,50 @@
-let arr = [0, 10, 5, 2];
+let arr = [0, 2, 1, 0];
 
-let peakIndex = Math.max(...arr);
+// let peakIndex = Math.max(...arr);
 
-let peakREs = 0;
-console.log(peakIndex);
+// let peakREs = 0;
+// console.log(peakIndex);
 
-for (let i = 0; i < arr.length; i++) {
-  if (arr[i] === peakIndex) {
-    peakREs = i;
-    break;
+// for (let i = 0; i < arr.length; i++) {
+//   if (arr[i] === peakIndex) {
+//     peakREs = i;
+//     break;
+//   }
+// }
+
+// console.log(peakREs);
+
+// let peakIndex = arr.reduce(
+//   (accu, curr, index) => {
+//     if (curr > accu.largest) {
+//       accu.largest = curr;
+//       accu.indexValue = index;
+//     }
+//     return accu;
+//   },
+//   { largest: 0, indexValue: 0 }
+// );
+
+// console.log(peakIndex.indexValue);
+
+let i = 0;
+let j = arr.length - 1;
+
+while (i < j) {
+  let mIndex = i + Math.floor((j - i) / 2);
+
+console.log(mIndex);
+
+
+  if (arr[mIndex] < arr[mIndex + 1]) {
+    i = mIndex + 1;
+  } else {
+    j = mIndex;
   }
+
+  // i++;
+  // j--;
 }
 
-console.log(peakREs);
+// console.log(i);
+// console.log(j);
